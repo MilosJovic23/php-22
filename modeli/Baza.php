f90bc820c2c571e4c39418eac0bab7d0dfe9062f
MilosJovic23 <milos.mj992@gmail.com>
2024-11-20 11:05:00 +0100

    * php 22 homework

--- modeli/Baza.php
+++ modeli/Baza.php
@@ -1,20 +1,16 @@
 <?php
 
 
+
+
     class Baza  {
 
-
         public $sql;
-        public $host="localhost";
-        public $user="root";
-        public $pass="";
-        public $database="web_shop";
 
         public function __construct(){
 
-            $this->sql=mysqli_connect($this->host,$this->user,$this->pass,$this->database);
+            $this->sql=mysqli_connect("localhost","root","" ,"web_shop");
+
         }
-
-
 
     }
